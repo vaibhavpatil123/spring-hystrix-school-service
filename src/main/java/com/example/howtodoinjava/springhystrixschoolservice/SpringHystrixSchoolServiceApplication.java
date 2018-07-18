9a0a191ea1488c822c82b7ea788bdd123ab5078c
vaibhav <vaibhav.patil@netcracker.com>
2018-07-18 14:51:20 +0530

    July 18

---
+++ src/main/java/com/example/howtodoinjava/springhystrixschoolservice/SpringHystrixSchoolServiceApplication.java
@@ -0,0 +1,18 @@
+package com.example.howtodoinjava.springhystrixschoolservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+@EnableZuulProxy
+public class SpringHystrixSchoolServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHystrixSchoolServiceApplication.class, args);
+	}
+}
